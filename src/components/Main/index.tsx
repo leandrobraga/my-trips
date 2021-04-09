818bb5ce4f6b9b0cd76876631aa8d603372ca21a
3upfulltech <leandro.braga@3up.tech>
2021-04-08 22:50:47 -0300

    End module 1

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,18 @@
+import * as S from './styles';
+
 const Main = () => (
-  <main>
-    <h2>React avançado</h2>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um atomo e react avançado escrito ao lado"
+    />
+    <S.Title>React avançado</S.Title>
+    <S.Description>TypeScript, ReactJs, NextJs, ... </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Desenvolvedor em frente a telas com códigos"
+    />
+  </S.Wrapper>
 );
 
 export default Main;
