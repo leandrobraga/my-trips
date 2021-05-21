084b9ba822ae6e566e28a630910c7caeeb022eba
3upfulltech <leandro.braga@3up.tech>
2021-05-21 14:17:10 -0300

    Fi

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,17 @@
-import Main from 'components/Main';
+import dynamic from 'next/dynamic';
+import { InfoOutline } from '@styled-icons/evaicons-outline';
+
+import LinkWrapper from 'components/LinkWarpper';
+
+const Map = dynamic(() => import('components/Map'), { ssr: false });
 
 export default function Home() {
-  return <Main />;
+  return (
+    <>
+      <LinkWrapper href="/about">
+        <InfoOutline size={22} aria-label="About" />
+      </LinkWrapper>
+      <Map />
+    </>
+  );
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,17 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
+  :root {
+    --highligth: #e20e8d;
+    --background: #030518;
+    --white: #eeeeee;
+    --grey: #cccccc;
+    --container: 100rem;
+    --small: 1.5rem;
+    --medium: 3rem;
+    --large: 5rem;
+  }
+
   * {
     margin: 0;
     padding: 0;
@@ -9,6 +20,8 @@ const GlobalStyles = createGlobalStyle`
 
   html {
     font-size: 62.5%;
+    background: var(--background);
+    color: var(--white);
   }
 
   html, body, #__next {
@@ -18,6 +31,16 @@ const GlobalStyles = createGlobalStyle`
   body {
     font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
     Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
+
+  p,
+  a {
+    font-size: 2rem;
+    line-height: var(--medium)
+  }
+
+  a {
+    color: var(--highligth);
   }
 `;
 
